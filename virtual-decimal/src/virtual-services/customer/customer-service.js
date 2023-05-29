97019b56992d265104c0739e51c1e6983a1ae9c5
arshu431 <arsh.thakur@accenture.com>
2023-05-29 13:37:15 +0530

    changes

--- virtual-decimal/src/virtual-services/customer/customer-service.js
+++ virtual-decimal/src/virtual-services/customer/customer-service.js
@@ -1,111 +1,107 @@
-const mbHelper = require("../../shared/mountebank-helper");
-const settings = require("../../shared/settings");
-const responseTemplate = require("./response-template");
+const mbHelper = require('../../shared/mountebank-helper');
+const settings = require('../../shared/settings');
+const responseTemplate = require('./response-template');
 
 function addService() {
-  const stubs = [
-    {
-      predicates: [
+    const stubs = [
         {
-          and: [
-            { equals: { method: "GET" } },
-            { startsWith: { path: "/customers/" } },
-          ],
-        },
-      ],
-      responses: [
-        {
-          is: {
-            statusCode: 200,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: responseTemplate,
-          },
-          _behaviors: {
-            lookup: [
-              {
-                key: {
-                  from: "path",
-                  using: { method: "regex", selector: "/customers/(.*)$" },
-                  index: 1,
-                },
-                fromDataSource: {
-                  csv: {
-                    path: "data/customers.csv",
-                    keyColumn: "id",
-                  },
-                },
-                into: "${row}",
-              },
-            ],
-            shellTransform: "node transformResponse.js id",
-          },
-        },
-      ],
-    },
-  ];
+            predicates: [{
+                and: [
+                    { equals: { method: "GET" } },
+                    { startsWith: { "path": "/customers/" } }
+                ]
+            }],
+            responses: [
+                {
+                    is: {
+                        statusCode: 200,
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: responseTemplate
+                    },
+                    _behaviors: {
+                        lookup: [
+                            {
+                                "key": {
+                                    "from": "path",
+                                    "using": { "method": "regex", "selector": "/customers/(.*)$" },
+                                    "index": 1
+                                },
+                                "fromDataSource": {
+                                    "csv": {
+                                        "path": "data/customers.csv",
+                                        "keyColumn": "id"
+                                    }
+                                },
+                                "into": "${row}"
+                            }
+                        ],
+                        shellTransform: "node transformResponse.js id"
+                    }
+                }
+            ]
+        }
+    ];
 
-  const imposter = {
-    port: settings.customer_service_get_port,
-    protocol: "http",
-    stubs: stubs,
-  };
+    const imposter = {
+        port: settings.customer_service_get_port,
+        protocol: 'http',
+        stubs: stubs
+    };
 
-  return mbHelper.postImposter(imposter);
+    return mbHelper.postImposter(imposter);
 }
 
 function postService() {
-  const stubs = [
-    {
-      predicates: [
+    const stubs = [
         {
-          and: [
-            { equals: { method: "POST" } },
-            { startsWith: { path: "/customers/" } },
-          ],
-        },
-      ],
-      responses: [
-        {
-          is: {
-            statusCode: 200,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: responseTemplate,
-          },
-          _behaviors: {
-            lookup: [
-              {
-                key: {
-                  from: "path",
-                  using: { method: "regex", selector: "/customers/(.*)$" },
-                  index: 1,
-                },
-                fromDataSource: {
-                  csv: {
-                    path: "data/customers.csv",
-                    keyColumn: "id",
-                  },
-                },
-                into: "${row}",
-              },
-            ],
-            shellTransform: "node transformResponse.js id",
-          },
-        },
-      ],
-    },
-  ];
+            predicates: [{
+                and: [
+                    { equals: { method: "POST" } },
+                    { startsWith: { "path": "/customers/" } }
+                ]
+            }],
+            responses: [
+                {
+                    is: {
+                        statusCode: 200,
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: responseTemplate
+                    },
+                    _behaviors: {
+                        lookup: [
+                            {
+                                "key": {
+                                    "from": "path",
+                                    "using": { "method": "regex", "selector": "/customers/(.*)$" },
+                                    "index": 1
+                                },
+                                "fromDataSource": {
+                                    "csv": {
+                                        "path": "data/customers.csv",
+                                        "keyColumn": "id"
+                                    }
+                                },
+                                "into": "${row}"
+                            }
+                        ],
+                        shellTransform: "node transformResponse.js id"
+                    }
+                }
+            ]
+        }
+    ];
 
-  const imposter = {
-    port: settings.customer_service_post_port,
-    protocol: "http",
-    stubs: stubs,
-  };
+    const imposter = {
+        port: settings.customer_service_post_port,
+        protocol: 'http',
+        stubs: stubs
+    };
 
-  return mbHelper.postImposter(imposter);
+    return mbHelper.postImposter(imposter);
 }
 
 module.exports = { addService, postService };
--- virtual-decimal/transformResponse.js
+++ virtual-decimal/transformResponse.js
@@ -4,6 +4,6 @@ const args = process.argv;
 var response = JSON.parse(process.env.MB_RESPONSE)
 if(JSON.stringify(Object.values(response.body)).includes("${row}["+args[2]+"]")) {
     response.body = {"status": "Record Not Found"};
-    response.statusCode = 404
+    response.statusCode = 200
 }
 console.log(JSON.stringify(response));
